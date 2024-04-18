7e4f4d3104d0915009f361691d4fec9e8a8e876a
Praveen K Singh <c2praveen30jun@gmail.com>
2024-04-19 01:43:19 +0530

    Update concurrent_execution.py

--- azure_service_bus/queue/concurrent_execution.py
+++ azure_service_bus/queue/concurrent_execution.py
@@ -1,8 +1,8 @@
 import subprocess
 
 # Start script1.py and script2.py as separate processes
-process1 = subprocess.Popen(["python", "D:/MyStuffs/Code/azure/azure_service_bus/queue/azure_service_bus_consumer_1.py"])
-process2 = subprocess.Popen(["python", "D:/MyStuffs/Code/azure/azure_service_bus/queue/azure_service_bus_consumer_2.py"])
+process1 = subprocess.Popen(["python", "/queue/azure_service_bus_consumer_1.py"])
+process2 = subprocess.Popen(["python", "/queue/azure_service_bus_consumer_2.py"])
 
 # Wait for both processes to finish
 process1.wait()
